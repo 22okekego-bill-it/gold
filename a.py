6712dc6a73b59a86cfa7e4ae6a811076c9abbf09
22okekego-bill-it <22okekego@hazelwick.org>
2025-12-12 14:43:24 +0000

    12/12/2025

--- a.py
+++ a.py
@@ -1,6 +1,30 @@
-num=float(input("Enter your num"))
-num2=float(input("Enter your num"))
-if num>num2:
-    print(num2,num)
+Word=Gamer 
+Letter=input("Guess first letter") 
+Letter2=input("Guess secondletter") 
+Letter3=input("Guess third letter")   
+Letter4=input("Guess fourth letter")    
+Letter5=input("Guess lastletter")    
+if Lette
+rfWord[0] 
+print("correct") 
 else:
-    print(num,num2)
+Letter=input("Enter your first letter you got wrong:")
+if Letter2==Word[1]
+print("correct") 
+else:
+Letter=input("Enter second letter you got wrong:")
+if Letter3==Word[2]
+print("correct") 
+else:
+Letter=input("Enter third letter you got wrong:")
+if Letter4==Word[3]
+print("correct") 
+else: 
+Letter5=input("Enter the letter yougot wrong")
+if Letter5==Word[4]
+print("correct")
+else:
+Letter=input 
+
+
+ 
